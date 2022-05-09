58b60116a3336de992c6aa3c04d7878d569f6a26
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-09 22:57:06 +0600

    Initially navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Pages/Shared/NavBar';
 
 function App() {
   return (
     <div>
-      <h2 className='text-3xl text-green-500'>Hello doctors portal</h2>
-      <button class="btn btn-primary">Button</button>
+      <NavBar></NavBar>
     </div>
   );
 }
