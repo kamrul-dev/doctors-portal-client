5b84a75ac179a2411dc0480c43a176bd2d3908bd
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-10 00:54:31 +0600

    banner text updated

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
                 <img src={chair} alt="doctor chair" className="max-w-xl rounded-lg shadow-2xl flex justify-end" />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolorem repudiandae sequi aspernatur, rerum quisquam cum ea suscipit tempore repellendus non fugiat nostrum nisi dicta deleniti, omnis quidem vel placeat.</p>
                     <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary uppercase text-white font-bold">Get Started</button>
                 </div>
             </div>
