b1974396c7ebf4cfb5ce879a35579ba520a96c2e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-11 04:01:39 +0600

    Create Booking form inside Booking modal1~

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,18 +1,35 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
-    const { name, slots } = treatment;
+const BookingModal = ({ treatment, setTreatment, date }) => {
+    const { _id, name, slots } = treatment;
+
+    const hadndleBooking = event => {
+        event.preventDefault();
+        const slot = event.target.slot.value;
+        console.log(slot, _id, name);
+        setTreatment(null);
+    }
+
     return (
         <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <label for="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div className="modal-action">
-                        <label for="booking-modal" className="btn">Yay!</label>
-                    </div>
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="font-bold text-center text-lg text-secondary">Booking for: {name}</h3>
+                    <form onSubmit={hadndleBooking} className='grid grid-cols-1 gap-4 justify-items-center mt-3'>
+                        <input type="text" value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" disabled />
+                        <select name="slot" className="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot => <option value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input name="name" type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                        <input name="email" type="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+                        <input name="phone" type="text" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value="Submit" placeholder="Type here" className="btn btn-secondary w-full max-w-xs" />
+                    </form>
                 </div>
             </div>
         </div>
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const { name, slots } = service
     return (
-        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl mt-24">
             <div className="card-body">
-                <h2 className="card-title text-secondary">{name}</h2>
-                <p>{
+                <h2 className="card-title justify-center text-secondary">{name}</h2>
+                <p className='text-center'>{
                     slots.length ? <span>{slots[0]}</span> : <span className='text-red-500'>Try Anoter Date</span>
                 }</p>
-                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <p className='text-center'>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
                 <div className="card-actions justify-center">
                     <label
                         disabled={slots.length === 0}
                         onClick={() => setTreatment(service)}
-                        for="booking-modal" class="btn btn-secondary text-white uppercase">Book Appointment</label>
+                        htmlFor="booking-modal" className="btn btn-secondary text-white uppercase">Book Appointment</label>
                 </div>
             </div>
         </div>
