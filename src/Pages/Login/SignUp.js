9c67977200e4497021c7e95b459b76a12d401555
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-14 22:01:02 +0600

    Save Registered user information in the database

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -15,6 +16,9 @@ const SignUp = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const [token] = useToken(user || gUser);
+
     const navigate = useNavigate()
 
     let signInError;
@@ -27,8 +31,8 @@ const SignUp = () => {
         signInError = <p className='text-red-500'><small>{error?.message || gError?.message || updateError?.message}</small></p>
     }
 
-    if (user || gUser) {
-        console.log(user);
+    if (token) {
+        navigate('/appointment');
     }
 
     const onSubmit = async data => {
@@ -36,8 +40,6 @@ const SignUp = () => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log('update done');
-        navigate('/appointment')
-
     }
     return (
         <div className='flex justify-center items-center h-screen'>
