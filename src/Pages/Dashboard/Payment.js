a57adfca8377b551b2904c6d9b8c1487c14a7d19
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-20 16:29:22 +0600

    Load appointment specific data install stripe and stripe for react

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
 const Payment = () => {
-    const {id} = useParams()
+    const { id } = useParams();
+    const url = `http://localhost:5000/booking/${id}`;
+
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h2 className="text-2xl text-purple-500">Please Pay for: {id}</h2>
+            <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div className="card-body">
+                    <p className='font-bold text-secondary'>Hello, {appointment.patientName}</p>
+                    <h2 className="card-title">Please pay for {appointment.treatment}</h2>
+                    <p>Your Appointment <span className='text-orange-500'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay : ${appointment.price}</p>
+                </div>
+            </div>
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
+
+                </div>
+            </div>
         </div>
     );
 };
