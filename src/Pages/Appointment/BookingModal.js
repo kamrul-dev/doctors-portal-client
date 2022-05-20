0cd903b65df518578352160007e2ba1923bff03a
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-20 15:20:18 +0600

    Database booking price added

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const BookingModal = ({ treatment, setTreatment, date, refetch }) => {
-    const { _id, name, slots } = treatment;
+    const { _id, name, slots, price } = treatment;
 
     const [user, loading, error] = useAuthState(auth);
 
@@ -20,6 +20,7 @@ const BookingModal = ({ treatment, setTreatment, date, refetch }) => {
             treatment: name,
             date: formattedDate,
             slot,
+            price,
             patient: user.email,
             patientName: user.displayName,
             phone: event.target.phone.value
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Service = ({ service, setTreatment }) => {
-    const { name, slots } = service
+    const { name, slots, price } = service
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body">
@@ -10,6 +10,7 @@ const Service = ({ service, setTreatment }) => {
                     slots.length ? <span>{slots[0]}</span> : <span className='text-red-500'>Try Anoter Date</span>
                 }</p>
                 <p className='text-center'>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <p className='text-center'><small>Price: ${price}</small></p>
                 <div className="card-actions justify-center">
                     <label
                         disabled={slots.length === 0}
