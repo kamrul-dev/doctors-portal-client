ff11c676636065bae082c17d6ac847b3c42afc34
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-20 17:02:15 +0600

    Basic Stipe setup and with simple card form

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L1T26DUkxPeu4jRuSvEWjhcnYIi1qeKESoT4jkJXvZ01QTLvPlui7vEwtAzgfkIPL8ptTm9THxIPxSxFnQMNMTR00ivHlyjez');
+
 
 const Payment = () => {
     const { id } = useParams();
@@ -29,7 +35,9 @@ const Payment = () => {
             </div>
             <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div className="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
