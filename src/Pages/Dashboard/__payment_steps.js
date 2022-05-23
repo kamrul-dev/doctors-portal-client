09ed8c673c50b1692337c6a9fd28fbf284d03acc
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-23 11:22:21 +0600

    Handle payment processing error and success~

---
+++ src/Pages/Dashboard/__payment_steps.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Installl stripe react JS
+ * 2. Open stripe account on stripe website
+ * 3. get publishable key pk
+ * 4. Create Elements wrapper using publishable key
+ * 5. Create Checkout form using card element, usestripe, useElements
+ * 6. get card elements info(credit card info)
+ * -------------------------------------------------------
+ * 7. get credit card info/error.
+ * 8. 
+ * **/
